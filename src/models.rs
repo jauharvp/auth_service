1ab657b4c1df23b9d0d5462c7d8ea48649d35d56
jauharvp <jauharvp@gmail.com>
2023-07-08 14:43:25 +0530

    db models

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,32 @@
 use diesel::{r2d2::ConnectionManager, PgConnection};
+use serde::{Desrialize, Serialize};
+use super::schema::*;
 
 //type alias to reduce verbosity
 
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+#[derive(Debug, Serialize, Desrialize, Queryable, Insertable)]
+#[table_name = "confirmations"]
+
+pub struct Confirmation {
+    pub id: Uuid,
+    pub email: String,
+    pub expires_at: chrono::NaiveDateTime,
+}
+
+
+#[derive(Debug,Serialize,Desrialize, Queryable, Insertable)]
+#[table_name = "users"]
+pub struct User {
+    pub id: Uuid,
+    pub email:String,
+    pub hash: String,
+    pub created_at: chrono::NaiveDateTime,
+}
+
+#derive[Debug,Serialize,Desrialize]
+pub struct SessionUser{
+    pub id: Uuid,
+    pub email:String,
+}
