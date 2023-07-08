09b0638edc1dd49d5d052b15e06c4ff7d93df157
jauharvp <jauharvp@gmail.com>
2023-07-08 14:52:21 +0530

    Implement models

--- src/models.rs
+++ src/models.rs
@@ -30,3 +30,33 @@ pub struct SessionUser{
     pub id: Uuid,
     pub email:String,
 }
+
+//Any typer that implementsd into<Strings> can be used to create a Confirmation
+impl<T> From<T> for Confirmation where
+T: Into<String> {
+    fn from(email: T) -> Self {
+        Confirmation {
+            id: Uuid::new_v4(),
+            email: email.into(),
+            expires_at: chrono::Local::now().naive_local() + chrono::Duration::hours(24),
+        }
+    }
+}
+
+
+imp From<User> for SessionUser {
+    fn from(User { email, id, ..}: User) -> Self {
+        SessionUser {email, id}
+    }
+}
+
+impl User {
+    pub fn  from<S: Into<String>, T: Into<String>>(email: S, pwd: T) -> Self {
+        User {
+            id: Uuid::new_v4(),
+            email: email.into(),
+            hash: pwd.into(),
+            created_at: chrono::Local::now().naive_local(),
+        }
+    }
+}
